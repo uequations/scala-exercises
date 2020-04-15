5ace970582032b479fc3c41da10ad7e1bfb53e6b
malkebu-lan <malkebu-lan@uequations.com>
2020-04-15 02:12:45 -0400

    Additional test cases added

--- src/test/scala/exercise/service/LongestContCovServiceSpec.scala
+++ src/test/scala/exercise/service/LongestContCovServiceSpec.scala
@@ -7,7 +7,7 @@ class LongestContCovServiceSpec extends FlatSpec with Matchers {
 
   behavior of "LongestContCovServiceSpec"
 
-  it should "apply" in {
+  it should "apply-1" in {
 
     val coverages: List[Cov] = List(
       Cov(21, 30), Cov(15, 25), Cov(28, 40), Cov(50, 60), Cov(61, 200),Cov(1,20))
@@ -15,4 +15,19 @@ class LongestContCovServiceSpec extends FlatSpec with Matchers {
     LongestContCovService(coverages) shouldEqual Cov(50,200)
   }
 
+  it should "apply-2" in {
+
+    val coverages: List[Cov] = List(
+      Cov(76, 156), Cov(77, 239), Cov(104, 263), Cov(146, 292), Cov(301, 340))
+
+    LongestContCovService(coverages) shouldEqual Cov(76,292)
+  }
+
+  it should "apply-3" in {
+
+    val coverages: List[Cov] = List(
+      Cov(35, 47), Cov(67, 76), Cov(85, 108), Cov(107, 314), Cov(272, 328),Cov(303,348))
+
+    LongestContCovService(coverages) shouldEqual Cov(85,348)
+  }
 }
