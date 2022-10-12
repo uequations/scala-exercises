f0294e1bcec3f461d881deb5d1f9c44fe92d1404
Mensah Alkebu-Lan <malkebu-lan@uequations.com>
2022-10-12 09:52:43 -0400

    more exercises

--- src/main/scala/exercise/closure/SimpleClosure.scala
+++ src/main/scala/exercise/closure/SimpleClosure.scala
@@ -5,9 +5,11 @@ object SimpleClosure {
   def main(args: Array[String]) = {
     val employeeNum = 50
 
-    def doIt(name: String) = {}
-
     val simpleClosure = (name: String) => println(s"Company name is $name" + s" and total no. of employees are $employeeNum")
+
+    def doIt(name: String) = {
+      simpleClosure(name)
+    }
 
     doIt("William")
   }
